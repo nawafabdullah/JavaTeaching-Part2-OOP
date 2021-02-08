220ab540e4caf05dbc6b848851a052bcdbf9ad35
Nawaf Abdullah <nawaf.manage@gmail.com>
2021-02-08 16:45:49 +0300

    update classes

--- First-Class/ClassesAndObjects/Game.java
+++ First-Class/ClassesAndObjects/Game.java
@@ -10,32 +10,45 @@ public class Game {
 
     public static void main(String[] args) {
 
-        // Creates a reader instance which takes
-        // input from standard input - keyboard
+        System.out.println("please enter the following for the FIRST player");
+        String[] playerOneInputs = ExtractInputs();
+        System.out.println("\nGreat! Now to the SECOND player ");
+        String[] playerTwoInputs = ExtractInputs();
+
+        Player firstPlayer = new Player(playerOneInputs[0], Double.parseDouble(playerOneInputs[1]),
+                Double.parseDouble(playerOneInputs[2]));
+
+        Player secondPlayer = new Player(playerTwoInputs[0], Double.parseDouble(playerTwoInputs[1]),
+                Double.parseDouble(playerTwoInputs[2]));
+
+        System.out.println(" Player's name: " + firstPlayer.playerName + "\n Player's Total Life Power: "
+                + firstPlayer.totalPower + "\n Player's Attack Power: " + firstPlayer.attackPower);
+
+        Players_Actions attack = new Players_Actions();
+
+        while (true) {
+            // System.out.println ("attacking is happening");
+            attack.attack(firstPlayer, secondPlayer);
+        }
+    }
+
+    public static String[] ExtractInputs() {
+
         Scanner reader = new Scanner(System.in);
         String[] playerParams = { "Name", "Total Life Power", "Attack Power" };
 
-        Player firstPlayer = new Player();
-        Player secondPlayer = new Player();
-
+        String[] userParams = new String[3];
         String userInput;
         for (int i = 0; i < playerParams.length; i++) {
-            System.out.println("please enter the player's " + playerPrams[i]);
-
+            System.out.println("player's " + playerParams[i]);
             userInput = reader.nextLine();
-            if (i == 0)
-                firstPlayer.playerName = userInput;
-            else if (i == 1)
-                firstPlayer.totalPower = Double.parseDouble(userInput);
-            else
-                firstPlayer.attackPower = Double.parseDouble(userInput);
-            ;
+            userParams[i] = userInput;
         }
-
-        System.out.println("Player's name: " + firstPlayer.playerName + "\n Player's Total Life Power: "
-                + firstPlayer.totalPower + "\n Player's Attack Power: " + firstPlayer.attackPower);
-
+        // reader.nextLine();
+        // reader.close();
+        return userParams;
     }
+
 }
 
 class Player {
@@ -52,8 +65,8 @@ class Player {
 
 class Players_Actions {
     public static void attack(Player first, Player second) {
+        System.out.println(" a fight is happening between:" + first.playerName + " And: " + second.playerName);
         double fightOutCome = (first.attackPower - second.totalPower);
-
         System.out.println("fight outcome is:" + fightOutCome);
         second.totalPower = (second.totalPower - fightOutCome);
     }
